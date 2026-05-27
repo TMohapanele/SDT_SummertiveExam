a58abfa510baa994bcdc3ec9b6ac73ce9921f480
TSHIRE <168409719+GLOSSYYY@users.noreply.github.com>
2026-05-27 10:26:34 +0200

    Completed Question 1

---
+++ SectionB_Q1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SectionB_Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: "); // Prompt the user to enter their name
+            string name = Console.ReadLine(); // Read the user's input and store it in the variable 'name'
+
+            Console.Write($"Hello {name}"); // Display a greeting message that includes the user's name using string interpolation
+        }
+    }
+}
